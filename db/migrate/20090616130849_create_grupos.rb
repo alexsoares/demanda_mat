04880778dc9703c981d9cdb7ba66f6b6f5b17e45
Alexandre <alexandre@arthur-desktop.(none)>
2009-07-23 14:35:18 -0300

    Alexandre - Acertando migrations Banco de Dados - 230709 14:35

--- db/migrate/20090616130849_create_grupos.rb
+++ db/migrate/20090616130849_create_grupos.rb
@@ -5,17 +5,12 @@ class CreateGrupos < ActiveRecord::Migration
       t.string :descricao, :limit => 50
       t.timestamps
     end
-    Grupo.create :nome => "Berçário I"
-    Grupo.create :nome => "Berçário II"
-    Grupo.create :nome => "Berçário III"
-    Grupo.create :nome => "Maternal I"
-    Grupo.create :nome => "Maternal II"
+    Grupo.create :nome => "Berçário I", :descricao => "Crianças de 4 meses à 1 ano "
+    Grupo.create :nome => "Berçário II", :descricao => "Crianças de 1 ano e 1 mes à 1 ano e 7 meses"
+    Grupo.create :nome => "Berçário III", :descricao => "Crianças de 1 ano e 8 meses à 2 anos e 4 meses"
+    Grupo.create :nome => "Maternal I", :descricao => "Crianças de 2 anos e 5 meses à 3 anos"
+    Grupo.create :nome => "Maternal II", :descricao => "Crianças de 3 anos e 1 mes à 3 anos e 6 meses"
 
-    Grupo.create :descricao => "Crianças de 4 meses à 1 ano "
-    Grupo.create :descricao => "Crianças de 1 ano e 1 mes à 1 ano e 7 meses"
-    Grupo.create :descricao => "Crianças de 1 ano e 8 meses à 2 anos e 4 meses"
-    Grupo.create :descricao => "Crianças de 2 anos e 5 meses à 3 anos"
-    Grupo.create :descricao => "Crianças de 3 anos e 1 mes à 3 anos e 6 meses"
   end
 
   def self.down
