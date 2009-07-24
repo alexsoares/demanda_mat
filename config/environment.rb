bb92ff1fe2d178e75d355a41cdbaf6f2f7b17959
seduc <seduc@seduc-desktop.(none)>
2009-07-24 15:28:49 -0300

    naor_alexandre_24_07_09_03

--- config/environment.rb
+++ config/environment.rb
@@ -73,4 +73,7 @@ Rails::Initializer.run do |config|
   # Please note that observers generated using script/generate observer need to have an _observer suffix
   # config.active_record.observers = :cacher, :garbage_collector, :forum_observer
 
+
+config.i18n.default_locale = "pt-BR"
+
 end
