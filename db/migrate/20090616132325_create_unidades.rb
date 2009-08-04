5adef0db60140a3aff2db4c3cb4f3761ae6f3c7d
seduc <seduc@seduc-desktop.(none)>
2009-08-04 10:38:26 -0300

    naor_alexandre_04_08_09INICIAL1

--- db/migrate/20090616132325_create_unidades.rb
+++ db/migrate/20090616132325_create_unidades.rb
@@ -8,116 +8,60 @@ class CreateUnidades < ActiveRecord::Migration
       t.timestamps
     end
 # Tipos:
-    # 1 - Creche
+    # 1 - CRECHE
     # 2 - EMEI
-    # 3 - Casas da Criança
+    # 3 - CASA DA CRIANÇA
     # 4 - EMEF
     # 5 - CIEP
+    # 6 - TEMPO DE VIVER
 
 # São Jerônimo
-    Unidade.create :nome => "ARACY", :tipo => 2, :regiao_id => 1
+    Unidade.create :nome => "ARAPIRANGA", :tipo => 3 , :regiao_id => 1
+    Unidade.create :nome => "BITU", :tipo => 3, :regiao_id => 1
+    Unidade.create :nome => "MAIRA", :tipo => 3, :regiao_id  => 1
+    Unidade.create :nome => "JURITI", :tipo => 3, :regiao_id => 1
     Unidade.create :nome => "JACY", :tipo => 2, :regiao_id => 1
-
+    Unidade.create :nome => "ARACY", :tipo => 2, :regiao_id => 1
+    Unidade.create :nome => "CAIC", :tipo => 1, :regiao_id => 1
 # Cidade Jardim
+    Unidade.create :nome => "GRAUNA", :tipo => 3, :regiao_id => 2
+    Unidade.create :nome => "ARAUNA", :tipo => 3, :regiao_id => 2
     Unidade.create :nome => "CARANDA", :tipo => 2, :regiao_id => 2
     Unidade.create :nome => "MANAI", :tipo => 2, :regiao_id => 2
     Unidade.create :nome => "ARACATI", :tipo => 2, :regiao_id => 2
     Unidade.create :nome => "CUNHATAI", :tipo => 2, :regiao_id => 2
-    Unidade.create :nome => "GRAUNA", :tipo => 3, :regiao_id => 2
-
+    Unidade.create :nome => "MARACA", :tipo => 1, :regiao_id => 2
+    Unidade.create :nome => "ANAJA", :tipo => 1, :regiao_id => 2
+    Unidade.create :nome => "CHUI", :tipo => 1, :regiao_id => 2
 # São Vito
+    Unidade.create :nome => "MANACA", :tipo => 3, :regiao_id => 3
     Unidade.create :nome => "POTIRA", :tipo => 2, :regiao_id => 3
     Unidade.create :nome => "PATURI", :tipo => 2, :regiao_id => 3
     Unidade.create :nome => "INDAIA", :tipo => 2, :regiao_id => 3
     Unidade.create :nome => "BATUIRA", :tipo => 2, :regiao_id => 3
     Unidade.create :nome => "JACINA", :tipo => 2, :regiao_id => 3
     Unidade.create :nome => "CURIMA", :tipo => 1, :regiao_id => 3
-
+    Unidade.create :nome => "MAJOI", :tipo => 1, :regiao_id => 3
+    Unidade.create :nome => "TAPERA", :tipo => 1, :regiao_id => 3
 # Zanaga
     Unidade.create :nome => "CURIO", :tipo => 3, :regiao_id => 4
     Unidade.create :nome => "TARAGUA", :tipo => 3, :regiao_id => 4
+    Unidade.create :nome => "URUPE", :tipo => 3, :regiao_id => 4
     Unidade.create :nome => "PATATIVA", :tipo => 2, :regiao_id => 4
     Unidade.create :nome => "ARACARI", :tipo => 2, :regiao_id => 4
     Unidade.create :nome => "CURUMIM", :tipo => 2, :regiao_id => 4
-
 # Jardim Ipiranga
     Unidade.create :nome => "TAHIRA", :tipo => 3, :regiao_id => 5
     Unidade.create :nome => "BORE", :tipo => 2, :regiao_id => 5
     Unidade.create :nome => "CECI", :tipo => 2, :regiao_id => 5
     Unidade.create :nome => "CORIMBO", :tipo => 2, :regiao_id => 5
     Unidade.create :nome => "TANGARA", :tipo => 2, :regiao_id => 5
-
 # Centro
     Unidade.create :nome => "BACURI", :tipo => 2, :regiao_id => 6
     Unidade.create :nome => "SABIA", :tipo => 2, :regiao_id => 6
-
-# Jardim Brasil
-    Unidade.create :nome => "TARAGUA", :tipo => 3, :regiao_id => 7
-
 # Praia Azul
-    Unidade.create :nome => "PITANGA", :tipo => 3, :regiao_id => 9
-    Unidade.create :nome => "PANAMBY", :tipo => 3, :regiao_id => 9
-
-# JARDIM DA PAZ
-   Unidade.create :nome => "ARAPIRANGA", :tipo => 3 , :regiao_id => 10
-
-# JARDIM DOS LIRIOS
-    Unidade.create :nome => "ARAUNA", :tipo => 3, :regiao_id => 11
-
-# PARQUE DAS NACOES
-      Unidade.create :nome => "BITU", :tipo => 3, :regiao_id => 12
-
-# SAO ROQUE
-  Unidade.create :nome => "JURITI", :tipo => 3, :regiao_id => 13
-    
-# PARQUE GRAMADO
-  Unidade.create :nome => "MAIRA", :tipo => 3, :regiao_id  => 14
-
-# JARDIM PROGRESSO
-  Unidade.create :nome => "MANACA", :tipo => 3, :regiao_id => 15
-
-# PROFILURB
-  Unidade.create :nome => "URUPE", :tipo => 3, :regiao_id => 16
-
-# JARDIM LILASES
-  Unidade.create :nome => "ANAJA", :tipo => 1, :regiao_id => 17
-
-# PARQUE LIBERADADE
-    Unidade.create :nome => "CAIC", :tipo => 1, :regiao_id => 18
-
-# MATHIENSEN
-    Unidade.create :nome => "CHUI", :tipo => 1, :regiao_id => 19
-
-# CORDENUCI
-    Unidade.create :nome => "MAJOI", :tipo => 1, :regiao_id => 20
-
-# JARDIM SÃO PEDRO
-   Unidade.create :nome => "MARACA", :tipo => 1, :regiao_id => 21
-
-# SAO MONOEL
-    Unidade.create :nome => "TAPERA", :tipo => 1, :regiao_id => 22
-
-
-
-
-    
-
-
-   
-
-
-
-
-
-
-
-    
-
-
-
-
-
-
+    Unidade.create :nome => "PITANGA", :tipo => 3, :regiao_id => 7
+    Unidade.create :nome => "PANAMBY", :tipo => 3, :regiao_id => 7
 
   end
 
