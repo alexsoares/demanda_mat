dc4d34571af2194888a7ff94b2bfc72ff51444ef
Alexandre Soares <alexandre@alexandre-desktop.(none)>
2010-01-28 10:50:08 -0200

    Finalização acertos solicitados, com autenticação e autorização III - 28012010 1049

--- db/migrate/20090616130849_create_grupos.rb
+++ db/migrate/20090616130849_create_grupos.rb
@@ -10,6 +10,9 @@ class CreateGrupos < ActiveRecord::Migration
     Grupo.create :nome => "BERCARIO III", :descricao => "CRIANCAS DE 1 ANO E 8 MESES A 2 ANOS E 4 MESES"
     Grupo.create :nome => "MATERNAL I", :descricao => "CRIANCAS DE 2 ANOS E 5 MESES A 3 ANOS"
     Grupo.create :nome => "MATERNAL II", :descricao => "CRIANCAS DE 3 ANOS E 1 MES A 3 ANOS E 6 MESES"
+    Grupo.create :nome => "NIVEL I", :descricao => "CRIANCAS DE 3 ANOS E 7 MESES A 4 ANOS E 6 MESES "
+    Grupo.create :nome => "NIVEL II", :descricao => "CRIANCAS DE 4 ANOS E 7 MESES A 5 ANOS E 6 MESES "
+
 
   end
 
